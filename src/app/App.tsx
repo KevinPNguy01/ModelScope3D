4e2b3d0b1557a0b0ab9121a62391bed6b3ae0f1d
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-12-19 18:10:41 -0800

    Set up router for web app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,15 @@
-import { useState } from "react";
-import { ImportButton } from "../features/import_menu/components/ImportButton";
-import { Canvas } from "../features/renderer/components/Canvas";
-import { ControlPanel } from "../features/transform/components/ControlPanel";
-import { FileContext } from "./contexts/FileContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { WebApp } from "./pages/WebApp";
 
 function App() {
-  const [objFile, setObjFile] = useState<File | null>(null);
-  const [mtlFile, setMtlFile] = useState<File | null>(null);
-  const [stlFile, setStlFile] = useState<File | null>(null);
 
-  return (
-    <FileContext.Provider value={{objFile, setObjFile, mtlFile, setMtlFile, stlFile, setStlFile}}>
-      <div className="flex flex-col">
-      <div className="flex bg-secondary">
-        <ImportButton/>
-      </div>
-      <div className="flex">
-        <Canvas/>
-        <ControlPanel/>
-      </div>
-    </div>
-    </FileContext.Provider>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/web" element={<WebApp/>}/>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
---
+++ src/app/pages/WebApp.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { ImportButton } from "../../features/import_menu/components/ImportButton";
+import { Canvas } from "../../features/renderer/components/Canvas";
+import { ControlPanel } from "../../features/transform/components/ControlPanel";
+import { FileContext } from "../contexts/FileContext";
+
+export function WebApp() {
+    const [objFile, setObjFile] = useState<File | null>(null);
+    const [mtlFile, setMtlFile] = useState<File | null>(null);
+    const [stlFile, setStlFile] = useState<File | null>(null);
+
+    return (
+        <FileContext.Provider value={{objFile, setObjFile, mtlFile, setMtlFile, stlFile, setStlFile}}>
+            <div className="flex flex-col">
+            <div className="flex bg-secondary">
+                <ImportButton/>
+            </div>
+            <div className="flex">
+                <Canvas/>
+                <ControlPanel/>
+            </div>
+            </div>
+        </FileContext.Provider>
+    );
+}
