5e5c3e90057ae00631384851e39052936d5baf34
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-12-26 23:17:45 -0800

    Enabled texture wrapping

--- src/utils/models/mtl.ts
+++ src/utils/models/mtl.ts
@@ -100,12 +100,21 @@ function loadTexture(gl: WebGLRenderingContext, path: string) {
 
     const image = new Image();
     image.onload = () => {
+        const isWidthPowerOfTwo = (image.width & (image.width - 1)) === 0;
+        const isHeightPowerOfTwo = (image.height & (image.height - 1)) === 0;
+
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        if (isWidthPowerOfTwo && isHeightPowerOfTwo) {
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+        } else {
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        }
+        
     };
     image.src = "./models/" + path;
 
